c153b926f05ab8992627384a7545ea08b788b0c5
Unbit <info@unbit.it>
2015-10-29 17:10:40 +0100

    added rectangle object

---
+++ aivengine/RectangleObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aiv.Engine
+{
+	public class RectangleObject : GameObject
+	{
+		public int width;
+		public int height;
+		public Color color;
+
+		public override void Draw() {
+			base.Draw ();
+			this.engine.workingGraphics.DrawRectangle (new Pen (color), this.x, this.y, this.width, this.height);
+		}
+	}
+}
