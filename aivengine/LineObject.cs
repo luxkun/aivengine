564ef21f0240a59f23fbc2413220d00b3bde24ad
Unbit <info@unbit.it>
2015-11-04 10:24:15 +0100

    implemented #2

---
+++ aivengine/LineObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Aiv.Engine;
+using System.Drawing;
+
+namespace Aiv.Engine
+{
+	public class LineObject : GameObject
+	{
+		public int x2;
+		public int y2;
+		public Color color;
+
+		private Pen pen;
+
+		public override void Draw() {
+			base.Draw ();
+			if (pen == null)
+				pen = new Pen (color);
+			this.engine.workingGraphics.DrawLine (pen, this.x, this.y, this.x2, this.y2);
+		}
+	}
+
+	public class RayObject : GameObject
+	{
+		
+		public int length;
+
+		public Color color;
+
+		protected Pen pen;
+
+		public override void Draw() {
+			base.Draw ();
+			if (pen == null)
+				pen = new Pen (color);
+		}
+	}
+
+	public class HorizontalRayObject : RayObject {
+		
+		public override void Draw() {
+			base.Draw ();
+			this.engine.workingGraphics.DrawLine (this.pen, this.x, this.y, this.x + length, this.y);
+		}
+	}
+
+	public class VerticalRayObject : RayObject {
+
+		public override void Draw() {
+			base.Draw ();
+			this.engine.workingGraphics.DrawLine (this.pen, this.x, this.y, this.x, this.y + length);
+		}
+	}
+}
+
